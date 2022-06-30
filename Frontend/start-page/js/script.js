4ab98e8f5a22da455f99b1c9abcd05ae78ff0edb
Aditya <adityakadali00@gmail.com>
2022-06-30 20:28:53 +0530

    Day 25

---
+++ Frontend/start-page/js/script.js
@@ -0,0 +1,16 @@
+// weather API
+const url = "https://api.chucknorris.io/jokes/random";
+async function joke() {
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let joke = data.value;
+      //   console.log(joke);
+      return joke;
+    })
+    .then((joke)=> {
+      document.querySelector('.temp').innerHTML = joke;
+
+    });
+}
+joke();
