4dbd1f5dc0eae8d0c22813a5964e179684a4e8fa
Aditya <adityakadali00@gmail.com>
2022-07-03 00:12:35 +0530

    added list items

--- Frontend/todoApp/scripts/script.js
+++ Frontend/todoApp/scripts/script.js
@@ -1,7 +1,9 @@
 function Todo() {
   var todo = document.querySelector(".form-data").value;
   console.log(todo);
-  
+  const li = document.createElement("li");
+  li.innerText = todo;
+  document.querySelector(".todo-items").appendChild(li);
 }
 
 const btn = document.querySelector("#btn");
